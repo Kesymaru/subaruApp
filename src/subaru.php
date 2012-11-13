02c6da7fe86cff3485f429e134be17f668532ce4
Andrey Alfaro Alvarado <andreyalfar@gmail.com>
2012-11-13 15:44:47 -0600

    facebook
    incluye metodo para manipular facebook y datos

--- src/subaru.php
+++ src/subaru.php
@@ -1,19 +1,65 @@
 <?php
-
 require_once('classes/classDatabase.php');
+require_once("../facebook/facebook.php");
 
 //Utilizar solo en modo produccion
 error_reporting(E_ALL ^ E_NOTICE);
 session_start();
 
 class Subaru{
+	private $datos = array();
+
+	/*
+	* Se encarga de todo lo relacionado con los permisos de facebook
+	*/
 	public function __construct(){
+		//inicia app para facebook
+		$facebook = new Facebook(array(
+		   'appId' => '533752239970800',
+		   'secret' => '3514c9cc9d9dab2606330ac6211ddae2',
+		   'cookie' => true,
+		));
+			
+		$user = $facebook->getUser();
+
+		if ($user) {
+			try {
+				$user_profile = $facebook->api('/me');
+			} catch (FacebookApiException $e) {
+				error_log($e);
+				$user = null;
+			} 
+		}
+
+		//no logueado -> pagina de logueo
+		if ( !$user ) {
+
+			$_SESSION['logueado'] = false;
+
+			//prmisos solicitados para la app
+			$loginUrl = $facebook->getLoginUrl(array('scope' => 'email,publish_stream'));
+
+			if(!$_SESSION['logueado']){	
+				echo "<script type='text/javascript'>top.location.href = '".$loginUrl."';</script>";		
+			}
+
+		}else{
+			//logueado
+			echo 'Logueado';
+			$_SESSION['logueado'] = true;
+			
+			//datos del usuario
+			$this->datos = array('nombre' => $user_profile['name'] , 'correo' => $user_profile['email'], 'id' => $user_profile['id'] );
+		}
 
 	}
 
-	//realiza la peticion de permisos y obtencion de datos del usuario
-	public function permisos(){
-
+	/*
+	* devuelve datos usario
+	* return datos array
+	*/
+	public getDatos(){
+		return $this->datos;
 	}
 
 	/*
@@ -34,7 +80,7 @@ class Subaru{
 			
 			//registra en la base de datos
 			$registro->queryInsert('participantes', $datos);
-			
+
 			echo 'Registrdo existosamente. Invita amigos para obtener puntos.';
 		}else{
 			echo 'Ya te has registrado, Invita amigos para obtener puntos.';
